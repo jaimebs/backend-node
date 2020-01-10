efd805d688818343e95630290431edc6d1f7ac2d
Jaime Barbosa <jaime.barbosa@solsis.br>
2020-01-10 16:48:02 -0300

    feat: first commit.

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import cors from 'cors'
+import express from 'express'
+
+import router from './router'
+
+const app = express()
+
+// Permite acesso externo
+app.use(cors())
+// Desativa o X-Powered-By: Express
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+app.use(router)
+
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port: ${process.env.PORT}`))
